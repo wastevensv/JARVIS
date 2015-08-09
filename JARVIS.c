196d1961efd7ce1802071533333d125a8b6a5538
William Stevens <contact@wastevensv.com>
2015-08-09 11:15:33 -0400

    Fixed indents and removed unused variable

--- JARVIS.c
+++ JARVIS.c
@@ -4,33 +4,34 @@
 
 void execstring(char com[])
 {
-        char response[256];
-        bzero(response,sizeof(&response));
-        char word[256];
+  char response[256];
+  bzero(response,sizeof(&response));
+
+  char word[256];
+  bzero(word,sizeof(&word));
+
+  char tmp[256];
+
+  int i=0;
+  int wc=0;
+
+  char c;
+  do {
+    c = com[i];
+    switch(c) {
+      case ' ':
+      case '\0':
+        sprintf(tmp, "%d: %s\n", wc, word);
+        strcat(response, tmp);
+        wc++;
         bzero(word,sizeof(&word));
-        char tmp[256];
-        
-        int i=0;
-        int wc=0;
-        int cc=0;
-        
-        char c;
-        do {
-                c = com[i];
-                switch(c) {
-                        case ' ':
-                        case '\0':
-                                sprintf(tmp, "%d: %s\n", wc, word);
-                                strcat(response, tmp);
-                                wc++;
-                                bzero(word,sizeof(&word));
-                                cc=0;
-                                break;
-                        default:
-                                strncat(word, &c, 1);
-                                break;
-                }
-                i++;cc++;
-        } while(c != '\0');
-        printf(response);
+        break;
+      default:
+        strncat(word, &c, 1);
+        break;
+    }
+  i++;
+  } while(c != '\0');
+
+  printf(response);
 }
--- main.c
+++ main.c
@@ -4,28 +4,29 @@
 
 int main (int argc, char *argv[])
 {
-        char c='\0';
-        char command[256];
-        bzero(command, sizeof(&command));
+  char c='\0';
+  char command[256];
+  bzero(command, sizeof(&command));
         
-        char *response;
-        
-        printf("JARVIS> ");
+  char *response;
 
-        while(c != EOF) {
-                c = getchar();
-                switch(c) {
-                        case '\n':
-                                printf("In: %s\n",command);
-                                execstring(command);
-                                bzero(command, sizeof(&command));
-                                printf("JARVIS> ");
-                                break;
-                        default:
-                                strncat(command, &c, 1);
-                                break;
-                }
-        }
-        return 0;
+  printf("JARVIS> ");
+
+  while(c != EOF) {
+    c = getchar();
+    switch(c) {
+      case '\n':
+        printf("In: %s\n",command);
+        execstring(command);
+        bzero(command, sizeof(&command));
+        printf("JARVIS> ");
+        break;
+      default:
+        strncat(command, &c, 1);
+        break;
+    }
+  }
+
+  return 0;
 }
 
